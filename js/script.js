dbd07d37c1a70c1f3958ebb73b37f8dd62bdb41b
Jack Turcotte <159164296+JackTurcs@users.noreply.github.com>
2024-05-30 14:54:47 +0000

    donezo

--- js/script.js
+++ js/script.js
@@ -4,14 +4,19 @@ function Submit(){
   let result = document.getElementById('Result');
   let current = num
   //factorial loop
-  do {
-    //incrimenting down
-    current-=1
-    //checking if we can multiply
-    if (current>0){
-      num*=current //multiplying num
-    }
-  } while(current>0)
-  //set display
-  result.innerHTML = num
+  if (num>0&&Math.round(num)==num){
+    do {
+      //incrimenting down
+      current-=1
+      //checking if we can multiply
+      if (current>0){
+        num*=current //multiplying num
+      }
+    } while(current>0)
+    //set display
+    result.innerHTML = num
+  } else{    
+    //set display
+    result.innerHTML = "Please enter a whole, positive number"
+  }
 }
